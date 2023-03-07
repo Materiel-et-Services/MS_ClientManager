daff7d8ccd36eae0144ece78a2c093ba03770b2d
bruno.sosin <bruno.sosin@matservgf.com>
2023-03-07 14:09:30 +0000

    System generated commit, after creating a new branch: newLogin

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -1,24 +1,10 @@
 export default {
-	getToken: async () => {
-		if (appsmith.URL.queryParams.code != undefined) {
-			console.log(appsmith.URL.queryParams)
-			await storeValue("code",appsmith.URL.queryParams.code,false)
-			console.log(appsmith.store.code)
-			let response =  await getToken.run({code: appsmith.store.code})
-			console.log(response)
-			await storeValue("jwt",response.access_token,false)
-			let params = appsmith.URL.queryParams.state.split("&")
-			let objectParams = {}
-			params.map((element) => {
-				let itemObject = element.split("=")
-				let newObj = {}
-				newObj[itemObject[0]] = itemObject[1]
-				Object.assign(objectParams,newObj)
-			})
+	getAuthUser: async () => {
+		if (getUser.data.id != undefined) {
 			
-			navigateTo("Page1",objectParams)
+			navigateTo("Page1",appsmith.URL.queryParams)
 		} else {
-			console.log("no")
+			showAlert("Un soucis c'est produit lors de l'authentification, essayer de vous relogger ou contacter votre administrateur")
 		}
 
 	}
