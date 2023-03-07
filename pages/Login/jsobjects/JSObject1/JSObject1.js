46decdbfb27a11e61cb14aee2594fc49aa43eccc
bruno.sosin <bruno.sosin@matservgf.com>
2023-03-07 15:10:44 +0000

    fix the bordel

--- pages/Login/jsobjects/JSObject1/JSObject1.js
+++ pages/Login/jsobjects/JSObject1/JSObject1.js
@@ -3,6 +3,7 @@ export default {
 		await getUser.run()
 		if (getUser.data != undefined) {
 			await showAlert("SUper")
+			await storeValue("userLogged",getUser.data,false)
 			navigateTo("Page1",appsmith.URL.queryParams)
 		} else {
 			showAlert("Un soucis c'est produit lors de l'authentification, essayer de vous relogger ou contacter votre administrateur")
